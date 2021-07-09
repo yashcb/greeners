9ea99eb0c41821173120f7f21a56c693518a4c04
Yash Chudasama <chudasamayash3004@.com>
2021-07-09 16:35:39 +0530

    Player animations and camera

--- Assets/Scripts/PhysicsObject.cs
+++ Assets/Scripts/PhysicsObject.cs
@@ -4,93 +4,104 @@ using UnityEngine;
 
 public class PhysicsObject : MonoBehaviour
 {
-    public float minGroundNormalY = 0.65f;
-    public float gravityModifier = 1.0f;
-   
+       public float minGroundNormalY = .65f;
+    public float gravityModifier = 1f;
+
     protected Vector2 targetVelocity;
-    protected Vector2 velocity;
+    protected bool grounded;
+    protected Vector2 groundNormal;
     protected Rigidbody2D rb2d;
+    protected Vector2 velocity;
     protected ContactFilter2D contactFilter;
     protected RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
-    protected List<RaycastHit2D> hitBufferList = new List<RaycastHit2D>(16); 
-     protected bool isGrounded;
-     protected Vector2 groundNormal;
-     protected const float minimumMoveDistance = 0.001f;
-     protected const float shellDistance = 0.01f;
-     
+    protected List<RaycastHit2D> hitBufferList = new List<RaycastHit2D> (16);
+
+
+    protected const float minMoveDistance = 0.001f;
+    protected const float shellRadius = 0.01f;
+
     void OnEnable()
     {
-       rb2d = GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D> ();
     }
-    // Start is called before the first frame update
-    void Start()
+
+    void Start () 
     {
         contactFilter.useTriggers = false;
-        contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask(gameObject.layer));
+        contactFilter.SetLayerMask (Physics2D.GetLayerCollisionMask (gameObject.layer));
         contactFilter.useLayerMask = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    void Update () 
     {
         targetVelocity = Vector2.zero;
-        ComputeVelocity();
+        ComputeVelocity ();    
     }
 
-    protected virtual void ComputeVelocity(){
+    protected virtual void ComputeVelocity()
+    {
 
     }
+
     void FixedUpdate()
-    {   
+    {
         velocity += gravityModifier * Physics2D.gravity * Time.deltaTime;
         velocity.x = targetVelocity.x;
-        isGrounded = false;
+
+        grounded = false;
 
         Vector2 deltaPosition = velocity * Time.deltaTime;
 
-        Vector2 moveAlongGround = new Vector2(groundNormal.x, -groundNormal.y);
+        Vector2 moveAlongGround = new Vector2 (groundNormal.y, -groundNormal.x);
+
         Vector2 move = moveAlongGround * deltaPosition.x;
-        Movement(move, false);
+
+        Movement (move, false);
 
         move = Vector2.up * deltaPosition.y;
-        Movement(move, true);
+
+        Movement (move, true);
     }
 
     void Movement(Vector2 move, bool yMovement)
     {
         float distance = move.magnitude;
-        if(distance > minimumMoveDistance){
 
-            int count = rb2d.Cast(move, contactFilter, hitBuffer, distance + shellDistance);
-            hitBufferList.Clear();
-
-            for (int i = 0; i < count; i++) // Add detected collisions to list
-            {
-                hitBufferList.Add(hitBuffer[i]);
+        if (distance > minMoveDistance) 
+        {
+            int count = rb2d.Cast (move, contactFilter, hitBuffer, distance + shellRadius);
+            hitBufferList.Clear ();
+            for (int i = 0; i < count; i++) {
+                hitBufferList.Add (hitBuffer [i]);
             }
 
-            for (int i = 0; i < hitBufferList.Count; i++) // Check normals
+            for (int i = 0; i < hitBufferList.Count; i++) 
             {
-                Vector2 currentNormal = hitBufferList[i].normal;
-                if (currentNormal.y > minGroundNormalY)
+                Vector2 currentNormal = hitBufferList [i].normal;
+                if (currentNormal.y > minGroundNormalY) 
                 {
-                    isGrounded = true;
-                    if (yMovement)
+                    grounded = true;
+                    if (yMovement) 
                     {
                         groundNormal = currentNormal;
                         currentNormal.x = 0;
                     }
                 }
-                float projection = Vector2.Dot(velocity, currentNormal);
-                if (projection < 0)
+
+                float projection = Vector2.Dot (velocity, currentNormal);
+                if (projection < 0) 
                 {
-                     velocity = velocity - projection * currentNormal;
+                    velocity = velocity - projection * currentNormal;
                 }
 
-                float modifiedDistance = hitBufferList[i].distance - shellDistance;
+                float modifiedDistance = hitBufferList [i].distance - shellRadius;
                 distance = modifiedDistance < distance ? modifiedDistance : distance;
             }
+
+
         }
+
         rb2d.position = rb2d.position + move.normalized * distance;
     }
+
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,21 +7,33 @@ public class PlayerController : PhysicsObject
     public float maxSpeed = 7;
     public float jumpTakeOffSpeed = 7;
 
-    
+  //private SpriteRenderer spriteRenderer;
+  private Animator animator;
+    void Awake(){
+      //  spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
     protected override void ComputeVelocity()
     {
         Vector2 move = Vector2.zero;
 
         move.x = Input.GetAxis ("Horizontal");
 
-        if (Input.GetButtonDown ("Jump") && isGrounded) {
-            velocity.y = jumpTakeOffSpeed;
-        } else if (Input.GetButtonUp ("Jump")) 
-        {
-            if (velocity.y > 0) {
-                velocity.y = velocity.y * 0.5f;
+            if (Input.GetButtonDown("Jump") && grounded)
+            {
+                velocity.y = jumpTakeOffSpeed;
             }
-        }
+
+        /* bool isFlipped = (spriteRenderer.flipX ? (move.x > 0.01 ) : (move.x < 0.01) );
+         if (isFlipped)
+         {
+             isFlipped = !isFlipped;
+         }*/
+
+        animator.SetBool("grounded", grounded);
+        animator.SetFloat("velocityX", Mathf.Abs(velocity.x )/ maxSpeed);
+        animator.SetFloat("velocityY", Mathf.Abs(velocity.y ));
         targetVelocity = move * maxSpeed;
     }
 }
