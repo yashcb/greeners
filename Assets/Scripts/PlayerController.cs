80c358cb0643b2ba114ad6482802a1b321cf67f3
Yash Chudasama <chudasamayash3004@.com>
2021-07-12 19:31:25 +0530

    Sprites polishes and character changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,18 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerController : PhysicsObject
 {
     public float maxSpeed = 7;
     public float jumpTakeOffSpeed = 7;
-    //private GameObject player;
-
-    //private SpriteRenderer spriteRenderer;
     private Animator animator;
+
     void Awake(){
-       // spriteRenderer = GetComponentInChildren<SpriteRenderer>();
-        animator = GetComponent<Animator>();
-        //player = GetComponent<GameObject>();
+        animator = GetComponent<Animator>();                
     }
 
     protected override void ComputeVelocity()
@@ -37,12 +34,6 @@ public class PlayerController : PhysicsObject
             characterScale.x = 1;
         }
         transform.localScale = characterScale;
-
-        /*bool isFlipped = (spriteRenderer.flipX ? (move.x > 0.01 ) : (move.x < 0.01) );
-        if (isFlipped)
-        {
-            isFlipped = !isFlipped;
-        }*/
 
         animator.SetBool("grounded", grounded);
         animator.SetFloat("velocityX", Mathf.Abs(velocity.x )/ maxSpeed);
