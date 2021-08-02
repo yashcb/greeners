341b86d31177b1989f92f8c93d281ee429ec45b7
Yash Chudasama <chudasamayash3004@gmail.com>
2021-08-02 23:46:39 +0530

    Improved AI behavior  (#8)
    * GRN-10 Basic AI
    
    * Polished and Working AI
    
    Co-authored-by: Yash Chudasama <chudasamayash3004@.com>

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -4,62 +4,58 @@ using UnityEngine;
 
 public class AIController : PhysicsObject
 {
-    [SerializeField] enum EnemyType {Bug, Zombie};
+    [SerializeField] enum EnemyType { Bug, Zombie };
     [SerializeField] EnemyType enemy;
     public float aiMaxSpeed = 7;
     public float aiDirection = 1;
     public float aiChangeDirectionEase = 1;
     private float aiDirectionSmooth = 1;
     private float playerDifference;
-    [SerializeField] private Animator _aiAnimator;
+    [SerializeField] private Animator aiAnimator;
 
     private bool followPlayer;
     public float followRange;
-    [SerializeField] private SpriteRenderer circleSelection;
-    // public float aijumpSpeed = 7;
-    //private bool isAIJumpping;
-    private Vector2 raycastOffset;
-    [SerializeField] private LayerMask layerMask;
+    [SerializeField] private Vector2 rayCastOffset;
+    //[SerializeField] private LayerMask layerMask;
     private RaycastHit2D leftLedge;
     private RaycastHit2D rightLedge;
 
-    private bool m_FacingRight;
-
     public void Start()
     {
-        _aiAnimator = GetComponent<Animator>();
-        circleSelection = GetComponentInChildren<SpriteRenderer>();
+        aiAnimator = GetComponent<Animator>();
     }
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(circleSelection.transform.position, followRange);
+        Gizmos.DrawWireSphere(transform.position, followRange);
     }
 
     protected override void ComputeVelocity()
     {
-        // Movement
+        // Direction
         Vector2 aiMove = Vector2.zero;
         playerDifference = PlayerController.Instance.gameObject.transform.position.x - transform.position.x;
         aiDirectionSmooth += (aiDirection - aiDirectionSmooth) * Time.deltaTime * aiChangeDirectionEase;
         aiMove.x = 1 * aiDirectionSmooth;
 
         // Flip sprite
-        if(aiMove.x > 0.01){
-        transform.localScale = 
-                new Vector3(-1, transform.localScale.y, transform.localScale.z);
+        if (aiMove.x > 0.01)
+        {
+            transform.localScale =
+                    new Vector3(-1, transform.localScale.y, transform.localScale.z);
         }
 
-        else if(aiMove.x < 0.01){
-        transform.localScale = 
-                new Vector3(1, transform.localScale.y, transform.localScale.z);
+        else if (aiMove.x < 0.01)
+        {
+            transform.localScale =
+                    new Vector3(1, transform.localScale.y, transform.localScale.z);
         }
 
         // Enemy type
-        if(enemy == EnemyType.Zombie)
+        if (enemy == EnemyType.Zombie)
         {
-            if (Mathf.Abs(playerDifference) < followRange)
+            if ((Mathf.Abs(playerDifference) < followRange))
             {
                 followPlayer = true;
             }
@@ -85,24 +81,25 @@ public class AIController : PhysicsObject
         if (!followPlayer)
         {
             // Left ledge
-            leftLedge = Physics2D.Raycast(new Vector2(transform.position.x - raycastOffset.x, transform.position.y), Vector2.down, 0.5f);
-            Debug.DrawRay(new Vector2(transform.position.x - raycastOffset.x, transform.position.y), Vector2.down, Color.blue, 60);
-            if(leftLedge.collider == null)
-            {
-                aiDirection = 1;
-            }
+            leftLedge =
+                Physics2D.Raycast(new Vector2(transform.position.x - rayCastOffset.x, transform.position.y), Vector2.down, .5f);
+            Debug.DrawRay(new Vector2(transform.position.x - rayCastOffset.x, transform.position.y), Vector2.down, Color.blue);
+
+            if (leftLedge.collider == null)
+            { aiDirection = 1; }
 
             // Right ledge
-            rightLedge = Physics2D.Raycast(new Vector2(transform.position.x + raycastOffset.x, transform.position.y), Vector2.down, 0.5f);
-            Debug.DrawRay(new Vector2(transform.position.x + raycastOffset.x, transform.position.y), Vector2.down, Color.blue, 60);
-            if(rightLedge.collider == null)
-            {
-                aiDirection = -1;
-            }
+            rightLedge =
+                Physics2D.Raycast(new Vector2(transform.position.x + rayCastOffset.x, transform.position.y), Vector2.down, .5f);
+            Debug.DrawRay(new Vector2(transform.position.x + rayCastOffset.x, transform.position.y), Vector2.down, Color.blue);
+
+            if (rightLedge.collider == null)
+            { aiDirection = -1; }
+
         }
 
-
-        _aiAnimator.SetFloat("velocityx", Mathf.Abs(velocity.x) / aiMaxSpeed);
+        // Movement
+        aiAnimator.SetFloat("velocityx", Mathf.Abs(velocity.x) / aiMaxSpeed);
         targetVelocity = aiMove * aiMaxSpeed;
     }
 }
