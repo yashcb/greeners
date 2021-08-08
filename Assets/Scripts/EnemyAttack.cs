4910daa243aebee2d8b5ba643a608ade4ef5b928
Yash Chudasama <chudasamayash3004@gmail.com>
2021-08-08 17:23:54 +0530

    GRN-12 Finishing enemy behaviour

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 6)
+        {
+            AIController.isAttacking = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 6)
+        {
+            AIController.isAttacking = false;
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 public class PlayerController : PhysicsObject
 {
+    private static PlayerController instance;
+    private bool isAttacking;
+    private Animator animator;
+
     public float maxSpeed = 7;
     public float jumpTakeOffSpeed = 7;
-    private Animator animator;
-    private bool isAttacking;
+    public int playerHealth;
+    public int playerMaxHealth;
 
-    void Awake() {
-        animator = GetComponent<Animator>();
-    }
-
-    private static PlayerController instance;
+    [SerializeField] GameObject attackHit;
     public static PlayerController Instance
     {
         get
@@ -23,40 +24,71 @@ public class PlayerController : PhysicsObject
             return instance;
         }
     }
+
+    private void Start()
+    {
+        playerHealth = playerMaxHealth;
+        animator = GetComponent<Animator>();
+    }
+
     protected override void ComputeVelocity()
     {
-        Vector2 move = Vector2.zero;
+       
+            Vector2 move = Vector2.zero;
 
-        move.x = Input.GetAxis ("Horizontal");
+            move.x = Input.GetAxis("Horizontal");
 
-        if (Input.GetButtonDown("Jump") && grounded)
-        {
-            velocity.y = jumpTakeOffSpeed;
-        }
+            if (Input.GetButtonDown("Jump") && grounded)
+            {
+                velocity.y = jumpTakeOffSpeed;
+            }
 
-        Vector3 characterScale = transform.localScale;
-        if (move.x < 0)
-        {
-            characterScale.x = -1;
-        } 
-        if (move.x > 0)
-        {
-            characterScale.x = 1;
-        }
-        transform.localScale = characterScale;
+            Vector3 characterScale = transform.localScale;
+            if (move.x < 0)
+            {
+                characterScale.x = -1;
+            }
+            if (move.x > 0)
+            {
+                characterScale.x = 1;
+            }
+            transform.localScale = characterScale;
 
+
+            if (Input.GetButtonDown("Fire1"))
+            {
+                isAttacking = true;
+            }
+            else if (Input.GetButtonUp("Fire1"))
+                isAttacking = false;
+
+            animator.SetBool("grounded", grounded);
+            animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
+            animator.SetFloat("velocityY", Mathf.Abs(velocity.y));
+            animator.SetBool("isAttacking", isAttacking);
+            targetVelocity = move * maxSpeed;
         
-        if (Input.GetButtonDown("Fire1"))
-        {
-            isAttacking = true;
-        }
-        else if(Input.GetButtonUp("Fire1"))
-            isAttacking = false;
+    }
 
-        animator.SetBool("grounded", grounded);
-        animator.SetFloat("velocityX", Mathf.Abs(velocity.x )/ maxSpeed);
-        animator.SetFloat("velocityY", Mathf.Abs(velocity.y ));
-        animator.SetBool("isAttacking", isAttacking);
-        targetVelocity = move * maxSpeed;
+    public void Hit()
+    {
+        if (playerHealth <= 0)
+        {
+            Die();
+        }
+        
+        Debug.Log(gameObject.name + " was hit!");
+        playerHealth -= 1;
+    }
+
+    public void Die()
+    {
+        Debug.Log("Player Died!");
+        animator.SetTrigger("hurt");
+        
+        gameObject.SetActive(false);
+        SceneManager.LoadScene("Gameplay");
+        //gameObject.GetComponent<Collider2D>().enabled = false;
+        //Destroy(gameObject);
     }
 }
